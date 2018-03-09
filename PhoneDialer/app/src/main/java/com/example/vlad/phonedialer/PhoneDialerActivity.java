c58526dedaa03c2c25c280980e830a923eb4065c
EIM 2017 <vladuttabusca@yahoo.com>
2018-03-09 17:36:01 +0200

    implemented dial a phone number

--- PhoneDialer/app/src/main/java/com/example/vlad/phonedialer/PhoneDialerActivity.java
+++ PhoneDialer/app/src/main/java/com/example/vlad/phonedialer/PhoneDialerActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class PhoneDialerActivity extends AppCompatActivity {
 
@@ -12,28 +13,31 @@ public class PhoneDialerActivity extends AppCompatActivity {
     StringBuilder phoneNumber;
     Button[] buttons;
     int index = 0;
+    EditText number;
+    ImageButton callButton;
+    ImageButton backspace;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_dialer);
 
-        Button callButton = (Button)findViewById(R.id.imageButton4);
-        Button backspace = (Button)findViewById(R.id.imageButton3);
-        EditText number = (EditText)findViewById(R.id.editText);
+        callButton = (ImageButton) findViewById(R.id.call_image_button);
+        backspace = (ImageButton)findViewById(R.id.backspace_image_button);
+        number = (EditText)findViewById(R.id.editText);
         phoneNumber = new StringBuilder(number.getText().toString());
 
         buttons = new Button[10];
         ids = new int[10];
-        ids[0] = R.id.button;
-        ids[1] = R.id.button2;
-        ids[2] = R.id.button2;
-        ids[3] = R.id.button3;
-        ids[4] = R.id.button4;
-        ids[5] = R.id.button5;
-        ids[6] = R.id.button6;
-        ids[7] = R.id.button7;
-        ids[8] = R.id.button8;
-        ids[9] = R.id.button9;
+        ids[0] = R.id.number_0_button;
+        ids[1] = R.id.number_1_button;
+        ids[2] = R.id.number_2_button;
+        ids[3] = R.id.number_3_button;
+        ids[4] = R.id.number_4_button;
+        ids[5] = R.id.number_5_button;
+        ids[6] = R.id.number_6_button;
+        ids[7] = R.id.number_7_button;
+        ids[8] = R.id.number_8_button;
+        ids[9] = R.id.number_9_button;
 
         int id;
         StringBuilder numbers;
@@ -45,16 +49,20 @@ public class PhoneDialerActivity extends AppCompatActivity {
             buttons[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    phoneNumber = phoneNumber.append(findViewById(ids[index++]).toString());
+                    phoneNumber = phoneNumber.append(((Button)v).getText());
+                    number.setText(phoneNumber);
                 }
             });
         }
-        index = 0;
 
         backspace.setOnClickListener(new View.OnClickListener(){
 
             public void onClick(View view){
-                phoneNumber = phoneNumber.deleteCharAt(phoneNumber.length()-1);
+                if(phoneNumber.length() != 0) {
+                    phoneNumber = phoneNumber.deleteCharAt(phoneNumber.length()-1);
+                    number.setText(phoneNumber);
+                }
+
             }
         });
 
